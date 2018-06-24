1fdec64a3f820146530482ac504acf868a8f50f2
Leandro Câmara <leandrocamara_eng@outlook.com>
2018-06-24 20:08:48 -0300

    UPDATE - Usando Template Forms na Compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { NgModule, LOCALE_ID } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
@@ -6,10 +7,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { ROUTES } from './app.router'
 
 import { RestaurantsService } from './restaurants/restaurants.service'
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service'
 
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
+import { OrderComponent } from './order/order.component'
 import { HeaderComponent } from './header/header.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
@@ -17,9 +20,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service'
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { OrderComponent } from './order/order.component'
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { OrderComponent } from './order/order.component'
   ],
   imports: [
     HttpModule,
+    FormsModule,
     BrowserModule,
     RouterModule.forRoot(ROUTES)
   ],
